1f4b4b8225e5da3b00c9692e2c179271fb99f058
Henry Burkhardt <henrywb04@gmail.com>
2023-09-27 10:14:41 -0500

    adding function strings

---
+++ ProductionCode/pyspill.py
@@ -0,0 +1,22 @@
+def load_dada():
+    """
+    Assigned: Feraidon 
+    """
+    return
+
+def lookup_by_company(company):
+    """
+    Assigned: Henry
+    """
+    return 
+
+def lookup_by_location(city, county, state):
+    """
+    Assigned: Paul
+    """
+    return
+
+
+# Command Line stuff - James
+
+
