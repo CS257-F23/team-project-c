e4e3a4bc7795e552634bf5f13ee5f959ffc8c8b2
CS Lab Account <cslab@olin308-02.mathcs.carleton.edu>
2023-10-08 11:42:26 -0500

    got the app to run

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from ProductionCode.pyspill import * 
 
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,16 +14,21 @@ def homepage():
     """
     return "welcome to PySpill"
 
-@app.route('/company/<input_name>', strict_slashes=False)
+@app.route('/<input_name>', strict_slashes=False)
 def company(input_name):
     """Show summary stats on a companies pipeline accidents, formatted in HTML string
 
     Args:
         input_name (str): Name of company to get data on
 
-    Returns:
+            Returns:
         str: simple HTML that displays the data
     """    
     load_data()
     _data = lookup_company(input_name)
     return render_template('lookupByCompany.html', company_name=input_name, data=_data)
+
+if __name__ == "__main__":
+    app.run()
+    main()
+
